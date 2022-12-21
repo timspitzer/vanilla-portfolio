375f6a5bffc79c44e966baf0b37319621ff2cea1
Tim Spitzer <tim.spitzer10@gmail.com>
2022-12-21 16:28:31 +0100

    added css effect

---
+++ script.js
@@ -0,0 +1,42 @@
+// TODO: replace local with session storage
+
+const element = document.querySelector(".overlay");
+let alreadyRemoved = JSON.parse(localStorage.getItem("alreadyRemoved"));
+
+const timeNow = new Date().getTime();
+const timeRemoved = JSON.parse(localStorage.getItem("timestampRemoved"));
+if (timeRemoved) {
+  // const waitTime = 1000 * 60 * 60 * 24 * 7;
+  const waitTime = 5000;
+  console.log("delay", timeNow - timeRemoved > waitTime);
+  if (timeNow - timeRemoved > waitTime) {
+    alreadyRemoved = false;
+    localStorage.setItem("alreadyRemoved", JSON.stringify(alreadyRemoved));
+  }
+}
+
+if (!alreadyRemoved) {
+  addBackground();
+  addRemoveBackgroundListener();
+}
+
+function addBackground() {
+  document.body.classList.add("disable-scroll");
+  element.classList.add("background-effect");
+  window.scroll(0, 0);
+}
+
+function addRemoveBackgroundListener() {
+  document.body.addEventListener(
+    "click",
+    (e) => {
+      e.preventDefault();
+      element.classList.remove("background-effect");
+      document.body.classList.remove("disable-scroll");
+
+      localStorage.setItem("alreadyRemoved", JSON.stringify(true));
+      localStorage.setItem("timestampRemoved", JSON.stringify(timeNow));
+    },
+    { once: true }
+  );
+}
