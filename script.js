b281fe8104d0cc3095b1eefe7ab6887a10afeeab
Tim Spitzer <tim.spitzer10@gmail.com>
2022-12-22 16:17:34 +0100

    switched to session storage

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-// TODO: replace local with session storage
-
+let alreadyRemoved = JSON.parse(sessionStorage.getItem("alreadyRemoved"));
 const element = document.querySelector(".overlay");
-let alreadyRemoved = JSON.parse(localStorage.getItem("alreadyRemoved"));
 
-const timeNow = new Date().getTime();
-const timeRemoved = JSON.parse(localStorage.getItem("timestampRemoved"));
-if (timeRemoved) {
-  const waitTime = 1000 * 60 * 60 * 24 * 7;
-  console.log("delay", timeNow - timeRemoved > waitTime);
-  if (timeNow - timeRemoved > waitTime) {
-    alreadyRemoved = false;
-    localStorage.setItem("alreadyRemoved", JSON.stringify(alreadyRemoved));
-  }
+if (alreadyRemoved) {
+  element.remove();
+  document.body.classList.remove("disable-scroll");
 }
 
 if (!alreadyRemoved) {
-  addBackground();
   addRemoveBackgroundListener();
-}
-
-function addBackground() {
-  document.body.classList.add("disable-scroll");
-  element.classList.add("background-effect");
-  window.scroll(0, 0);
 }
 
 function addRemoveBackgroundListener() {
@@ -32,9 +17,7 @@ function addRemoveBackgroundListener() {
       e.preventDefault();
       element.classList.remove("background-effect");
       document.body.classList.remove("disable-scroll");
-
-      localStorage.setItem("alreadyRemoved", JSON.stringify(true));
-      localStorage.setItem("timestampRemoved", JSON.stringify(timeNow));
+      sessionStorage.setItem("alreadyRemoved", JSON.stringify(true));
     },
     { once: true }
   );
