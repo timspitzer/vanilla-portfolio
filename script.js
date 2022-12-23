3b534dad64e91f2fbd4b2328ebabab8055aad3d6
Tim Spitzer <tim.spitzer10@gmail.com>
2022-12-23 22:58:22 +0100

    Changed animation to use mouse

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let alreadyRemoved = JSON.parse(sessionStorage.getItem("alreadyRemoved"));
 const overlay = document.querySelector(".overlay");
 const introText = document.querySelector("#short-intro-text");
 
+window.addEventListener("mousemove", (e) => {
+  const x = -1 * Math.round((e.clientX * 10) / window.innerWidth - 5) + "deg";
+  const y = -1 * Math.round((e.clientY * 10) / window.innerHeight - 5) + "deg";
+  // const x = Math.round((e.clientX * 10) / window.innerWidth - 5) + "deg";
+  // const y = Math.round((e.clientY * 10) / window.innerHeight - 5) + "deg";
+
+  document.body.style.setProperty("--mouse-x", x);
+  document.body.style.setProperty("--mouse-y", y);
+});
+
 if (alreadyRemoved) {
   overlay.remove();
   document.body.classList.remove("disable-scroll");
